cdcd3690366b142db772403b080ede54030f839a
Otávio Schwanck dos Santos <otavio.schwanck@gmail.com>
2015-10-09 13:13:18 -0300

    fix

--- app/models/mobile.rb
+++ app/models/mobile.rb
@@ -1,7 +1,8 @@
 class Mobile < ActiveRecord::Base
   belongs_to :budget
   has_many :plates
-
+  validates_presence_of :multiplier, :name
+  validates_numericality_of :multiplier
   def total_value
   	total_value = 0
   	self.plates.each do |plate|
